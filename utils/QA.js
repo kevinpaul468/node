2063e3576c9bcebfbc6c7943b0dee5e5e3268d18
kevin paul <kevinpaul468@gmail.com>
2024-03-26 12:43:13 +0530

    made changes

--- utils/QA.js
+++ utils/QA.js
@@ -6,17 +6,18 @@ const client = new MongoClient(databaseURL);
 
 async function addQuestion(email, question) {
     await client.connect();
-    const db = await client.db(database);
-    const collection = await db.collection('QA');
+    const db = client.db(database);
+    const collection = db.collection('QA');
     const document = await collection.insertOne({ email: email, question: question});
+    console.log(document);
     console.log('inserted successfully');
     return;
 }
 
 async function addComment(email,question,comment){
     await client.connect();
-    const db = await client.db(database);
-    const collection = await db.collection('comments');
+    const db = client.db(database);
+    const collection = db.collection('comments');
     const document = await collection.insertOne({ email: email, comment: comment});
     const commentId = document.insertedId;
     joinCommentToQuestion(question, commentId);
@@ -24,8 +25,8 @@ async function addComment(email,question,comment){
 
 async function joinCommentToQuestion(question, commentId){
     await client.connect();
-    const db = await client.db(database);
-    const collection = await db.collection('QA');
+    const db = client.db(database);
+    const collection = db.collection('QA');
     const document = await collection.updateOne({ question: question }, { $push: { comments: commentId } });
     console.log(document);
     console.log('joined comment to question');
@@ -34,13 +35,35 @@ async function joinCommentToQuestion(question, commentId){
 
 async function joinCommentToComment(comment, commentId){
     await client.connect();
-    const db = await client.db(database);
-    const collection = await db.collection('comments');
+    const db = client.db(database);
+    const collection = db.collection('comments');
     const document = await collection.updateOne({ comment: comment }, { $push: { comments: commentId } });
     console.log(document);
     console.log('joined comment to comment');
     return;
 }
 
+async function getQuestions(){
+    await client.connect();
+    const db = await client.db(database);
+    const collection = await db.collection('QA');
+    const document = await collection.find().toArray();
+    return document;
+}
 
-moduld.exports = { addQuestion, addComment, joinCommentToQuestion, joinCommentToComment };
+async function getComments(question){
+    await client.connect();
+    const db = client.db(database);
+    const collection = db.collection('QA');
+    const document = await collection.findOne({ question: question });
+    const commentIds = document.comments;
+    const comments = [];
+    for (let i = 0; i < commentIds.length; i++) {
+        const collection = db.collection('comments');
+        const comment = await collection.findOne({ _id: commentIds[i] });
+        comments.push(comment); 
+    }   
+    return comments;
+}
+
+module.exports = { addQuestion, addComment, joinCommentToQuestion, joinCommentToComment, getQuestions, getComments };
